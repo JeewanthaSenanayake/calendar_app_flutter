9b38fbf336196fbd74f7a8ca168c7b42396e308f
unknown <jeewanthamsenanayake@gmail.com>
2023-08-17 10:26:35 +0530

    Calendar

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:calendar/events_with_calander.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calendar',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: events_with_calander(),
+    );
+  }
+}
